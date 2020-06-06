46416f393949ce55f7c61e9399e949df683b738f
Dreadblade-dev <sqandex1suggestions@gmail.com>
2020-06-06 23:30:07 +0200

    Stack is refactored, added some unit tests

--- data_structures/stack.h
+++ data_structures/stack.h
@@ -1,6 +1,23 @@
 #pragma once
+#include <iostream>
 #include <exception>
-#include "stack_empty_exception.h"
+#include <cassert>
+#include <cstdint>
+
+/**
+ * @author Dreadblade- (https://github.com/Dreadblade-dev)
+ * Stack realization
+ * 
+ * In computer science, a stack is an abstract data type that serves as a collection of elements, with two principal operations:
+ *  • push, which adds an element to the collection, and
+ *  • pop, which removes the most recently added element that was not yet removed.
+ *
+ * The order in which elements come off a stack gives rise to its alternative name, LIFO (last in, first out). Additionally, a peek operation may give access to the top without modifying the stack.
+ *
+ * Source: https://en.wikipedia.org/wiki/Stack_(abstract_data_type)
+ */
+
+
 
 namespace constants
 {
@@ -18,25 +35,175 @@ private:
 	T* m_data;
 	
 public:
-	Stack(std::size_t capacity);
-	Stack(Stack<T>& stack);
+	Stack(std::size_t capacity = constants::defaultCapacity);
+	Stack(Stack& stack);
 	~Stack();
 
-	bool isEmpty();
+	bool isEmpty() noexcept;
 
-	void clear();
+	void clear() noexcept;
 
-	void reallocate(std::size_t capacity);
+	void reallocate(std::size_t capacity) noexcept;
 
-	void push_back(const T& item);
+	void push_back(const T& item) noexcept;
 
-	T& pop();
+	T pop();
 
-	T& top();
+	T peek();
 
-	std::size_t getSize();
+	std::size_t getSize() noexcept;
 
-	std::size_t getCapacity();
+	std::size_t getCapacity() noexcept;
 
-	Stack<T>& operator=(Stack<T>& s);
+	Stack& operator=(Stack& s) noexcept;
 };
+
+class StackEmptyException : public std::exception
+{
+public:
+	const char* what() const noexcept override
+	{
+		return "Stack is empty";
+	}
+};
+
+template <class T>
+Stack<T>::Stack(std::size_t capacity) : m_capacity(capacity), m_size(0)
+{
+	m_data = new T[capacity];
+}
+
+template <class T>
+Stack<T>::Stack(Stack<T>& s)
+{
+	m_size = s.m_size;
+	m_capacity = s.m_capacity;
+	m_data = new T[m_capacity];
+
+	for (std::size_t count = 0; count < m_size; ++count)
+	{
+		m_data[count] = s.m_data[count];
+	}
+}
+
+template <class T>
+Stack<T>::~Stack()
+{
+	delete[] m_data;
+}
+
+template <class T>
+bool Stack<T>::isEmpty() noexcept
+{
+	return (m_size == 0);
+}
+
+template <class T>
+void Stack<T>::clear() noexcept
+{
+	delete[] m_data;
+	m_data = nullptr;
+	m_size = 0;
+	m_capacity = 0;
+}
+
+template <class T>
+void Stack<T>::reallocate(std::size_t capacity) noexcept
+{
+	if (m_capacity == capacity)
+		return;
+
+	if (m_data == nullptr)
+	{
+		m_data = new T[capacity];
+		m_capacity = capacity;
+	}
+	else
+	{
+		T* data = new T[capacity];
+
+		if (m_capacity < capacity)
+		{
+			for (std::size_t count = 0; count < m_capacity; ++count)
+			{
+				data[count] = m_data[count];
+			}
+		}
+		else
+			if (m_capacity > capacity)
+			{
+				if (m_size > capacity)
+					m_size = capacity;
+				for (std::size_t count = 0; count < capacity; ++count)
+				{
+					data[count] = m_data[count];
+				}
+			}
+
+
+		m_capacity = capacity;
+		delete[] m_data;
+		m_data = data;
+	}
+}
+
+template <class T>
+void Stack<T>::push_back(const T& item) noexcept
+{
+	if ((m_size + 1) == m_capacity)
+		reallocate(m_capacity + constants::extendedCapacity);
+	m_data[m_size] = item;
+	++m_size;
+}
+
+template <class T>
+T Stack<T>::pop()
+{
+	if (isEmpty())
+		throw StackEmptyException();
+
+	T tmp = m_data[m_size - 1];
+	--m_size;
+
+	return tmp;
+}
+
+template <class T>
+T Stack<T>::peek()
+{
+	if (isEmpty())
+		throw StackEmptyException();
+
+	return m_data[m_size - 1];
+}
+
+template <class T>
+std::size_t Stack<T>::getSize() noexcept
+{
+	return m_size;
+}
+
+template <class T>
+std::size_t Stack<T>::getCapacity() noexcept
+{
+	return m_capacity;
+}
+
+template <class T>
+Stack<T>& Stack<T>::operator=(Stack<T>& s) noexcept
+{
+	if (this == &s)
+		return *this;
+
+	this->clear();
+	m_capacity = s.m_capacity;
+	m_size = s.m_size;
+
+	m_data = new T[m_capacity];
+	for (std::size_t count = 0; count < m_size; ++count)
+	{
+		m_data[count] = s.m_data[count];
+	}
+
+	return *this;
+}
---
+++ data_structures/test/stack_tests.cpp
@@ -0,0 +1,151 @@
+#include "../stack.h"
+#include <gtest/gtest.h>
+
+namespace StackTest
+{
+	TEST(StackTest, StackCreatesEmpty)
+	{
+		Stack<int16_t> stack;
+		EXPECT_EQ(stack.getSize(), 0);
+		EXPECT_TRUE(stack.isEmpty());
+	}
+
+	TEST(StackTest, StackCreatesWithNoData)
+	{
+		Stack<int16_t> stack;
+		EXPECT_THROW(stack.pop(), StackEmptyException);
+	}
+
+	TEST(StackTest, StackClears)
+	{
+		Stack<int16_t> stack;
+		for (int i = 0; i < 256; i++)
+		{
+			stack.push_back(i);
+		}
+		stack.clear();
+		EXPECT_TRUE(stack.isEmpty());
+	}
+	
+	TEST(StackTest, StackReallocate)
+	{
+		Stack<int16_t> stack;
+		for (int i = 0; i < 31; i++)
+		{
+			stack.push_back(i);
+		}
+		EXPECT_EQ(stack.getCapacity(), 32);
+		stack.push_back(1);
+		EXPECT_EQ(stack.getSize(), 32);
+		stack.push_back(1);
+		EXPECT_EQ(stack.getCapacity(), 48);
+	}
+
+	TEST(StackTest, StackReallocateExpand)
+	{
+		Stack<int16_t> stack;
+		for (int i = 0; i < 32; i++)
+		{
+			stack.push_back(i);
+		}
+
+		stack.reallocate(256);
+		EXPECT_EQ(stack.getSize(), 32);
+		EXPECT_EQ(stack.getCapacity(), 256);
+	}
+
+	TEST(StackTest, StackReallocateNarrow)
+	{
+		Stack<int16_t> stack;
+		for (int i = 0; i < 256; i++)
+		{
+			stack.push_back(i);
+		}
+
+		stack.reallocate(16);
+		EXPECT_EQ(stack.getCapacity(), 16);
+		EXPECT_EQ(stack.getSize(), 16);
+		EXPECT_EQ(stack.peek(), 15);
+	}
+
+	TEST(StackTest, StackEqualOperator)
+	{
+		Stack<int16_t> stack1;
+		Stack<int16_t> stack2 = stack1;
+		EXPECT_EQ(stack1.isEmpty(), stack2.isEmpty());
+		for (int i = 0; i < 256; i++)
+		{
+			stack1.push_back(i);
+		}
+
+		stack2 = stack1;
+		
+		EXPECT_EQ(stack1.getSize(), stack2.getSize());
+		EXPECT_EQ(stack1.getCapacity(), stack2.getCapacity());
+		EXPECT_EQ(stack1.peek(), stack2.peek());
+	}
+
+	TEST(StackTest, StackCopyConstructor)
+	{
+		Stack<int16_t> stack1;
+		Stack<int16_t> stack2(stack1);
+		EXPECT_EQ(stack1.isEmpty(), stack2.isEmpty());
+		for (int i = 0; i < 256; i++)
+		{
+			stack1.push_back(i);
+		}
+
+		Stack<int16_t> stack3(stack1);
+
+		EXPECT_EQ(stack3.getSize(), stack1.getSize());
+		EXPECT_EQ(stack3.getCapacity(), stack1.getCapacity());
+		EXPECT_EQ(stack3.peek(), stack1.peek());
+	}
+
+	TEST(StackTest, StackEmptyThrowsException)
+	{
+		Stack<int16_t> stack;
+		EXPECT_THROW(stack.pop(), StackEmptyException);
+		for (int i = 0; i < 32; i++)
+			stack.push_back(i);
+
+		for (int i = 0; i < 32; i++)
+			stack.pop();
+		
+		EXPECT_THROW(stack.pop(), StackEmptyException);
+	}
+
+	TEST(StackTest, StackPushBack)
+	{
+		Stack<int16_t> stack;
+
+		for (int i = 0; i <= 64; i++)
+		{
+			stack.push_back(i);
+			EXPECT_EQ(stack.pop(), i);
+		}
+			
+
+		EXPECT_TRUE(stack.isEmpty());
+	}
+
+	TEST(StackTest, StackPeek)
+	{
+		Stack<int16_t> stack;
+
+		for (int i = 0; i <= 64; i++)
+		{
+			stack.push_back(i);
+		}
+
+		for (int i = 64; i >= 0; i--)
+		{
+			stack.reallocate(i + 1);
+			EXPECT_EQ(stack.peek(), i);
+		}
+
+		stack.pop();
+		
+		EXPECT_TRUE(stack.isEmpty());
+	}
+}
